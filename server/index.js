e4e2dbf194aa89056638b26e438013fdeeeebda5
bajenoB <jonjill1488@gmail.com>
2022-12-24 00:27:57 +0200

    Added mongodb connect and user class

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT||3000
 const app=express()
 
 app.use(express.json());
-
+    
 
 
 
@@ -15,6 +15,12 @@ app.get('/',function(res,req){
     
     console.log("On server!");
 })
+
+mongoose.connect(process.env.mongooseConnection, function (err, db) {
+    if (!err) {
+      console.log("Mongose connected");
+    } else console.log(err);
+  });
 
 app.listen(PORT,function(){
     console.log("Server starts on 3000 PORT");
